df598b6670d4cbcba2c8f077f6a8b2eef1db7bd9
takashi <42316228+k-tokitoh@users.noreply.github.com>
2023-06-25 12:18:44 +0900

    nested routes

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
 #[get("/")]
 async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+    HttpResponse::Ok().body("<h1>Hello world! html</h1>")
 }
 
 #[post("/echo")]
@@ -11,7 +11,11 @@ async fn echo(req_body: String) -> impl Responder {
 }
 
 async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+    HttpResponse::Ok().body("<div style='color: red;'>Hey there!!!!")
+}
+
+async fn jump() -> impl Responder {
+    "jumped."
 }
 
 #[actix_web::main]
@@ -21,6 +25,9 @@ async fn main() -> std::io::Result<()> {
             .service(hello)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
+            .service(
+                web::scope("/hop").service(web::scope("/step").route("/jump", web::get().to(jump))),
+            )
     })
     .bind(("127.0.0.1", 8080))?
     .run()
